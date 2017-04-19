02d7fb6f96c26f18f44723ae95a38dd306e33e83
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-04-19 18:09:26 -0400

    Added InputParser#getOptions method and corresponding test.

--- spec/input-parser.spec.js
+++ spec/input-parser.spec.js
@@ -7,7 +7,7 @@
 var InputParser = require( '../lib/input-parser' );
 
 describe( 'Test "InputParser"', function() {
-	it ( 'Should return `undefined` when instantiated with an empty array.', function() {
+	it( 'Should return `undefined` when instantiated with an empty array.', function() {
 		try {
 			var inputParser = new InputParser( [] );
 		} catch ( err ) {
@@ -17,9 +17,16 @@ describe( 'Test "InputParser"', function() {
 		expect( typeof inputParser ).toBe( 'undefined' );
 	} );
 
-	it ( 'Should instantiate successfully when provided with a single, Array-type argument.', function() {
+	it( 'Should instantiate successfully when provided with a single, Array-type argument.', function() {
 		var inputParser = new InputParser( [ 1, 2, 3, 4, 5 ] );
 
 		expect( inputParser instanceof InputParser ).toBe( true );
+	} );
+
+	it( 'Should return an empty array if instantiated with an argument of insufficient length.', function() {
+		var inputParser = new InputParser( [ 'path/to/file' ] );
+		var options = inputParser.getOptions();
+
+		expect( Array.isArray( options ) && options.length === 0 ).toBe( true );
 	} );
 } );
