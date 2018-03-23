a8627acd6b9dd1096ff7c064b4c9949ba5376337
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-03-23 08:55:41 -0400

    topic-8 Update 'index.js' to be importable module.

--- index.js
+++ index.js
@@ -16,7 +16,6 @@
 const fs = require( 'fs' );
 
 // Vendor
-const meow = require( 'meow' );
 const recursive = require( 'recursive-readdir' );
 const Promise = require( 'bluebird' );
 
@@ -28,62 +27,40 @@ const OutputParser = require( './lib/output-parser' );
 /* -------------------------------------------------- */
 /* DECLARE VARS */
 /* -------------------------------------------------- */
-const cli = meow( {
-	help: `
-NAME
-	Frontburner
-
-SYNOPSIS
-	fbr [--display --excludes=<temp,logs> --prefixes=<baz,quux> --keywords=<foo,bar>] [path/to/file]
-
-EXAMPLES
-	fbr index.js
-	fbr .
-	fbr lib/index.js --display
-	fbr . --keywords=TODO,FIXME
-	fbr . --prefixes=@
-	fbr . --excludes=logs
-
-DESCRIPTION
-	Frontburner is designed to be run from the command line using the \`fbr\` command.
-
-	Start with the command, followed by the name of the file that you want to 'scan'. For example: \`fbr index.js\`.
-
-	To recursively scan a folder, provide the path. For example: \`fbr path/to/folder/\`.
-
-OPTIONS
-	--display
-	Suppress log file creation, print output to stdout.
-
-	--excludes=<temp,logs>
-	Override the files/folders which Frontburner excludes by default.
-
-	--prefixes=<baz,quux>
-	Override the default 'keyword prefixes' that Frontburner checks for.
-
-	--keywords=<foo,bar>
-	Override the default 'keywords' that Frontburner checks for.
-	`,
-} );
-
-const inputParser = new InputParser( cli.input, cli.flags );
-const fileParser = new FileParser();
-const outputParser = new OutputParser();
+let inputParser;
+let fileParser;
+let outputParser;
 
 /* -------------------------------------------------- */
 /* DECLARE FUNCTIONS */
 /* -------------------------------------------------- */
-function init() {
-	let target = null;
+function frontburner( args, opts ) {
+	// Ensure `args` data type is correct.
+	args = ( Array.isArray( args ) ) ? args : [ args ];
+
+	// Update global vars.
+	inputParser = new InputParser( args, opts );
+	fileParser = new FileParser();
+	outputParser = new OutputParser();
+
+	return new Promise( ( resolve, reject ) => {
+		init( args[ 0 ] )
+			.then( parse )
+			.then( log )
+			.then( resolve, reject );
+	} );
+}
+
+function init( target ) {
 	let filePaths = [];
 
 	return new Promise( function( resolve ) {
-		if ( !cli.input[ 0 ] ) {
+		if ( !target ) {
 			throw new Error( 'Whoops! Frontburner requires at least one argument.' );
 		}
 
 		// Prepend current working directory if input is not an absolute path.
-		target = ( cli.input[ 0 ].substring( 0, 1 ) !== '/' ) ? `${process.cwd()}/${cli.input[ 0 ]}` : cli.input[ 0 ];
+		target = ( target.substring( 0, 1 ) !== '/' ) ? `${process.cwd()}/${target}` : target;
 
 		// Handle cases where `target` is a folder.
 		if ( fs.lstatSync( target ).isDirectory() ) {
@@ -125,19 +102,18 @@ function parse( filePaths ) {
 }
 
 function log( output ) {
-	if ( inputParser.getOption( 'display' ) ) {
-		console.log( output );
-	} else {
-		outputParser.writeLog( output );
-	}
+	return new Promise( function( resolve ) {
+		if ( inputParser.getOption( 'display' ) ) {
+			console.log( output );
+		} else {
+			outputParser.writeLog( output );
+		}
+
+		resolve( output );
+	} );
 }
 
 /* -------------------------------------------------- */
-/* INIT */
+/* PUBLIC API */
 /* -------------------------------------------------- */
-init()
-	.then( parse )
-	.then( log )
-	.catch( ( err ) => {
-		console.log( err.message || err );
-	} );
+module.exports = frontburner;
